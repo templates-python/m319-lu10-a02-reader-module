4ed613754712ad7c7b60fad3f1b83598417a0aa5
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-12-04 13:36:32 +0100

    prepare input_reader.py

--- input_reader.py
+++ input_reader.py
@@ -1,34 +1,3 @@
 """
 input_reader module with input validation
 """
-
-def read_float(text_to_display):
-    """
-    read a float from the console
-    :param text_to_display:
-    :return:
-    """
-    while True:
-        try:
-            num = float(input(text_to_display))
-        except ValueError:
-            print("Please, enter a real number!")
-            continue
-        else:
-            return num
-
-
-def read_int(text_to_display):
-    """
-    read an int from the console
-    :param text_to_display:
-    :return:
-    """
-    while True:
-        try:
-            num = int(input(text_to_display))
-        except ValueError:
-            print("Please, enter a valid whole number!")
-            continue
-        else:
-            return num
